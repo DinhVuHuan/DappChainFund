a00ded4cb68578ef45d148c8ec40952dc2722c3b
DinhVuHuan <127218228+DinhVuHuan@users.noreply.github.com>
2025-11-23 23:24:16 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { TbAwardFilled } from 'react-icons/tb'
 const Header = () => {
     return (
-        <div>Header</div>
+        <header
+            className='flex justify-between items-center
+            p-5 bg-white text-gray-700 hover:text-gray-900
+            shadow-lg fixed top-0 left-0 right-0'
+        >
+            <a href="#">
+                <span className="text-lg font-medium">Dapp ChainFund</span>
+                <div className="flex space-x-1">
+                    <TbAwardFilled className="text-yellow-500 text-xl" />
+                    <TbAwardFilled className="text-yellow-500 text-xl" />
+                    <TbAwardFilled className="text-yellow-500 text-xl" />
+                    <TbAwardFilled className="text-yellow-500 text-xl" />
+                    <TbAwardFilled className="text-yellow-500 text-xl" />
+                </div>
+            </a>
+        </header>
     )
+
 }
 
 export default Header
